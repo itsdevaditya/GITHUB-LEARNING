b115798ee1829837f63906f48b8ce536e1b57e41
itsdevaditya <adigoyl05@gmail.com>
2024-05-05 19:57:03 +0530

    learn variable

--- learn_php/index.php
+++ learn_php/index.php
@@ -10,7 +10,21 @@
         This is my first php website
         <br>
         <?php
-        echo "Hello world and this is printed using php"
+        echo "Hello world and this is printed using php";
+        ?>
+        <br>
+          <?php
+       
+        // comments
+        /* multiline 
+        comment */
+        // variable 
+        $variable1 = 34;  
+        $variable2 = "hy";
+        echo $variable1;
+        echo "<br>";
+        echo $variable2;
+
         ?>
     </div>
     
